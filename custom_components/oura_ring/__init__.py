e096273eff8b9ad02d5c430e462fcded30e88e6d
0xvisualiris <59976822+0xvisualiris@users.noreply.github.com>
2024-10-19 00:45:11 +0200

    Update __init__.py

--- custom_components/oura_ring/__init__.py
+++ custom_components/oura_ring/__init__.py
@@ -10,9 +10,7 @@ async def async_setup(hass, config):
 
 async def async_setup_entry(hass, entry):
     """Set up Oura Ring from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
     return True
 
 async def async_unload_entry(hass, entry):
