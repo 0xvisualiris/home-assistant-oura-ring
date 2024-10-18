1c5ae601f46b3337e0decf04a131ce9f84aefc71
0xvisualiris <59976822+0xvisualiris@users.noreply.github.com>
2024-10-19 00:59:35 +0200

    Update __init__.py

--- custom_components/oura_ring/__init__.py
+++ custom_components/oura_ring/__init__.py
@@ -1,19 +1,24 @@
-import logging
+from __future__ import annotations
 
-_LOGGER = logging.getLogger(__name__)
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
 DOMAIN = "oura_ring"
+PLATFORMS = ["sensor"]
+_LOGGER = logging.getLogger(__name__)
 
-async def async_setup(hass, config):
-    """Set up the Oura Ring component."""
-    return True
-
-async def async_setup_entry(hass, entry):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Oura Ring from a config entry."""
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass, entry):
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    return True
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
